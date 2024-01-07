a532a0f5e97d921dce9069d0953b47f295d44312
JordanTzvetkov <149078174+JordanTzvetkov@users.noreply.github.com>
2024-01-07 13:34:59 +0000

    unique values storage for 3 of the columns selected

--- 01_Sulfotyrosine_Data_Processing/scripts/04_Aggregate_batch_data_bypeptidoformID.py
+++ 01_Sulfotyrosine_Data_Processing/scripts/04_Aggregate_batch_data_bypeptidoformID.py
@@ -6,10 +6,10 @@ import pandas as pd
 batch = ''
 # Get the directory where peptidoform_id data is stored
 data_to_process_folder = os.path.abspath(os.path.join(os.getcwd(), '..', 'out', batch))
-extension_to_find = '_ID.csv'
+extension_to_find = 'peptidoform_ID.csv'
 # extension_to_find = '_ID.csv'
 csv_files = LF.getfiles(folder=data_to_process_folder, extension=extension_to_find, get_prefix=False)
-
+print(csv_files)
 # we need absolute paths:
 csv_file_paths = [os.path.join(data_to_process_folder, csv_file) for csv_file in csv_files]
 
@@ -19,10 +19,10 @@ all_data = pd.concat([pd.read_csv(file) for file in csv_file_paths], ignore_inde
 # Step 3: Group by 'peptidoform_id' and aggregate data
 aggregated_data = all_data.groupby('peptidoform_id').agg({
     'calibrated_error': list,
-    'dataset_ID': list,
-    'protein': list,
-    'alt_protein': list
+    'dataset_ID': lambda x: list(set(x)),
+    'protein': lambda x: list(set(x)),
+    'alt_protein': lambda x: list(set(x))
 }).reset_index()
 
 # Step 4: Save the results as a CSV file
-aggregated_data.to_csv(f'../out/{batch}_aggregated_by_peptidoform_ID.csv', index=False)
+aggregated_data.to_csv(f'../out/{batch}_unique_aggregated_by_peptidoform_ID.csv', index=False)
--- 01_Sulfotyrosine_Data_Processing/scripts/troubleshooting.R
+++ 01_Sulfotyrosine_Data_Processing/scripts/troubleshooting.R
@@ -1,4 +1,66 @@
+library(stringr)
+
 wd <- "C:/Users/jtzve/Desktop/Sufo_Tyrosine/01_Sulfotyrosine_Data_Processing/scripts"
 setwd(wd)
 
-test <- read.csv("../out/")
+# read in the aggregated data
+test <- read.csv("../out/_agg")
+names(test)
+test1<-test
+# Convert the string representations of lists to actual lists and then apply 
+# unique to see where we're actually going wrong - are we recording all information in the
+# aggregated_by_peptidoform_ID.csv files? 
+
+# testing some stuff to write cleaning function in loop:
+# clean_string <- gsub("\\[|\\]|'", "", test[3,5]) 
+# clean_string <- gsub(";", ",", test[2,5])# Remove brackets and single quotes
+# elements <- unlist(strsplit(clean_string, ',\\s*'))  # Split into elements
+# unique(elements)
+# length(unique(elements))
+# i = 3
+
+
+# for columns that are not the peptidoform ID one
+for (i in 2:ncol(test)){
+  # Apply a cleaning function to:
+  cleaned_col <- lapply(test[,i], function(x) {
+    
+    # 1) # Remove brackets and single quotes from the string
+    clean_string <- gsub("\\[|\\]|'", "", x)  
+    
+    # 2) replace ; with , for the alt protein column (number 5)
+    clean_string <- gsub(";", ",", clean_string)
+    
+    # 3) # Split into elements at every comma removing any white space to follow it
+    elements <- unlist(strsplit(clean_string, ',\\s*'))
+    
+    # 4) return all elements
+    return(elements)
+      
+  })
+  
+  
+  # 5) Convert the list of vectors into a single character string per row
+  # by collapsing the elements
+  # for the calibrated mass errors column (number 2), retain all elements.
+  if (i==2){
+    
+    test[,i] <- sapply(cleaned_col, function(x) paste(x, collapse = ", "))
+    
+  } else {
+  # for all other columnsm reatin only the unique values
+  test[,i] <- sapply(cleaned_col, function(x) paste(unique(x), collapse = ", "))
+    
+  }
+  
+}
+
+
+# sanity check: how many peptidoforms were found in more than one dataset? 
+# easist way is to count how many rows contain a ',' in their 3rd column
+has_comma <- grepl(",", test$dataset_ID)
+sum(has_comma)
+
+## conclusion - b the looks of it our code that outputs all aggregated_by_peptidoform_ID.csv
+# funcitons as intended. The trouble we run into must be an artefact of subsequent
+# scripts.
